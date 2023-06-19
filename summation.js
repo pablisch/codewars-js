66ce9447eaebe1f5a2c5856e65bc4b3d87118d03
Pablo <pablito@me.com>
2023-06-19 12:56:56 +0100

    Added alternative syntax to summation

--- summation.js
+++ summation.js
@@ -1,7 +1,8 @@
 const summation = (num) => {
   // const arr = Array.from(Array(num).keys(), x => x + 1);
   // return arr.reduce((total, current) => total + current, 0)
-  return Array.from(Array(num).keys(), x => x + 1).reduce((total, current) => total + current, 0)
+  // return Array.from(Array(num).keys(), x => x + 1).reduce((total, current) => total + current, 0)
+  return [...Array(num).keys()].map( x => x + 1).reduce((total, current) => total + current, 0)
 }
 
 module.exports = summation;
