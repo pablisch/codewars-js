be072716dbe663af42ca36203e0322f4983d84be
Pablo <pablito@me.com>
2023-06-20 17:31:03 +0100

    Deleted misplaced dom-manipulation folder

--- 2.6_DOM-manipulation/index.js
+++
@@ -1,8 +0,0 @@
-const View = require("./view");
-
-const view = new View();
-view.clearParagraphs()
-// view.addParagraph("the business")
-// view.addParagraph("a teapot")
-// view.clearParagraphs
-
